1f88fe0e8492497789359c0a3051dfad22305e3f
landers <liaorenj@gmail.com>
2022-08-14 17:19:53 +0800

    update - 增加symbol符号

---
+++ symbol/symbol.go
@@ -0,0 +1,53 @@
+/*
+Create: 2022/8/14
+Project: FuShin
+Github: https://github.com/landers1037
+Copyright Renj
+*/
+
+// Package symbol
+package symbol
+
+// 常用的符号
+
+// 标点
+const (
+	SymbolDot       = "."
+	SymbolComma     = ","
+	SymbolQues      = "?"
+	SymbolQuote     = "'"
+	SymbolDQuote    = `"`
+	SymbolBQuote    = "`"
+	SymbolAnd       = "&"
+	SymbolExcal     = "!"
+	SymbolAt        = "@"
+	SymbolHash      = "#"
+	SymbolDol       = "$"
+	SymbolPercent   = "%"
+	SymbolUp        = "^"
+	SymbolStar      = "*"
+	SymbolSub       = "-"
+	SymbolAdd       = "+"
+	SymbolEqual     = "="
+	SymbolL         = "<"
+	SymbolR         = ">"
+	SymbolLVirgule  = "/"
+	SymbolRVirgule  = `\`
+	SymbolOr        = "|"
+	SymbolLBracket  = "["
+	SymbolRBracket  = "]"
+	SymbolLBrace    = "{"
+	SymbolRBrace    = "}"
+	SymbolUnder     = "_"
+	SymbolMiddleDot = "•"
+	SymbolRQues     = "¿"
+)
+
+const (
+	SymbolDots   = "..."
+	SymbolFrom   = "<-"
+	SymbolTo     = "->"
+	SymbolArrowL = "<="
+	SymbolArrowR = "=>"
+	SymbolCode   = "</>"
+)
